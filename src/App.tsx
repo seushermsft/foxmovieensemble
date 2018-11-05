767c41c363b4f8ea07cec4b40f29da5939d55650
Sean Usher <seusher@microsoft.com>
2018-11-05 15:58:47 -0700

    Add the ability to draw on top of the video so we can eventually draw bounding boxes

--- src/App.tsx
+++ src/App.tsx
@@ -52,17 +52,22 @@ class App extends Component {
     this.setState({ playingbackRate: 2 })
   }
 
+  onProgress = (state: { playedSeconds: number }) => {
+    // console.log('onProgress', state)
+    // We only want to update time slider if we are not currently seeking
+    //var canvas : any = document.getElementById("mycanvas");
+    var canvas : any = this.refs.boundingBoxCanvas
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0,0,150,100);
+    ctx.fillRect(state.playedSeconds,0,150,100);
+    ctx.fillStyle="#FF0000";
+  }
 
   // onDuration = (duration) => {
   //   console.log('onDuration', duration)
   //   this.setState({ duration })
   // }
-
-  // onProgress = state => {
-  //   console.log('onProgress', state)
-  // }
-
-
 
   render() {
     const { url, playing, volume, loaded, duration, playbackRate, played } = this.state
@@ -70,51 +75,63 @@ class App extends Component {
     return (
       <div className ='app'>
         <section className='section'>
-          <h1>ReactPlayer Demo</h1>
+          <div id="title">
+            <h1>ReactPlayer Demo</h1>
+          </div>
           <div className='player-wrapper'>
+            <div id="base">
             <ReactPlayer
-                className = 'react-player'
-                url = {sources.sintelTrailer} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                playing = {playing}
-                volume = {volume}
-                height = '75%'
-                width = '75%'
-                playbackRate = {playbackRate}
-                //onDuration ={this.onDuration}
-            />
-          </div>  
+                  className = 'react-player'
+                  url = {sources.sintelTrailer} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                  playing = {playing}
+                  volume = {volume}
+                  playbackRate = {playbackRate}
+                  onProgress={this.onProgress}
+                  //onDuration ={this.onDuration}
+              />
+            </div>
+            <div id="overlay">
+              <canvas ref="boundingBoxCanvas"></canvas>
+            </div>
+          </div>
+          <table id="controls">
+            <tbody>
+            <tr>
+                <td>
+                  <button onClick={this.playPause}> Play Pause Button</button>
+                  <button onClick={this.onPause}> Pause</button>
+                  <button onClick={this.onPlay}> Play</button>
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>
+                  <h6>Playback Speed</h6>
+                  <button onClick={this.setPlaybackRateSlow} value ={.5}>.5</button>
+                  <button onClick={this.setPlaybackRateFast} value ={1.5}>1.5</button>
+                </td>
+              </tr>
+            </tbody>
+            
+            <tbody>
+              <tr>
+                <th>
+                  <h2>State </h2>
+                </th>
+              </tr>
+              <tr>
+                <th>Seconds Elapsed:</th>
+                <td>{loaded}</td>
+              </tr>
+              <tr>
+                <th>Time Remaining:</th>
+                <td>{played}</td>
+              </tr>
+            </tbody>
+          </table>
         </section>      
-      <table>
-        <tbody>
-          <button onClick={this.playPause}> Play Pause Button</button>
-          <button onClick={this.onPause}> Pause</button>
-          <button onClick={this.onPlay}> Play</button>
-      
-        </tbody>
-        <tbody>
-          <tr>
-            <td>
-              <h6>Playback Speed</h6>
-              <button onClick={this.setPlaybackRateSlow} value ={.5}>.5</button>
-              <button onClick={this.setPlaybackRateFast} value ={1.5}>1.5</button>
-            </td>
-          </tr>
-         
-        
-        </tbody>
-        <h2>State </h2>
-        <tbody>
-          <tr>
-            <th>Seconds Elapsed:</th>
-            <td>{loaded}</td>
-          </tr>
-          <tr>
-            <th>Time Remaining:</th>
-            <td>{played}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      </div>
     ) 
   }
 }
